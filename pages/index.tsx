0c9cffa2cce6532f941dca97b069d2432fdba14a
ahonore42 <a1honore80@gmail.com>
2021-04-21 22:17:51 -0500

    add breakpoints for responsive styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,24 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+       
+          <section className="h-screen">
+            <div className="flex flex-wrap justify-around shadow-xl rounded-md bg-gray-900">
+          
+              <div className="object-center flex justify-center align-center no-wrap flex-col">
+                <h1 className="font-custom text-4xl xs:text-5xl sm:text-7xl lg:text-8xl xl:text-9xl text-center bg-clip-text text-transparent bg-gradient-to-b from-pink-600 to-indigo-700 font-black py-4 px-4">Adam Honore</h1>
+                <h2 className="font-custom text-2xl xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-center bg-clip-text text-transparent bg-gradient-to-b from-pink-600 to-indigo-700 font-black py-4 px-4 ">Full Stack Software Developer</h2>
+              </div>
+
+              <div className="box-content bg-gradient-to-b from-pink-600 to-indigo-700 rounded-full m-4"> 
+                <img src="https://res.cloudinary.com/ahonore42/image/upload/v1587911075/adam1_x3rxny.jpg" alt="Adam"
+                  className="w-64 inline-block align-middle rounded-full m-1"
+                />
+              </div>
+
+            </div>
+          </section>
+     
 
         <p className={styles.description}>
           Get started by editing{' '}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Royalacid'],
+        graf: ['WildBasic'],
+        dec: ['AgiesFresh']
+      },
+      backgroundSize: {
+       '50%': '50%',
+       '60%': '60%',
+       '70%': '70%',
+       '80%': '80%',
+       '90%': '90%',
+       '100%':'100%'
+      }
+    },
+    screens: {
+      'xs': '360px',
+      'sm': '600px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   variants: {
     extend: {},
